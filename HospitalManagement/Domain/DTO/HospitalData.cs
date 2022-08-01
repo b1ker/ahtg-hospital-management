9b6e2b06293ce5295423c0cb569d2394af907639
Pawel Kaniewski <pawel@kaniewski.eu>
2022-07-31 23:23:37 -0500

    Initial and final

---
+++ HospitalManagement/Domain/DTO/HospitalData.cs
@@ -0,0 +1,11 @@
+﻿namespace HospitalManagement.Domain.Entities
+{
+    public class Hospital
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid AddressId { get; set; }
+        public Address Address { get; set; }
+    }
+}
