e8b332ce0e1cba1d5332c1306c397c852d19812e
Pawel Kaniewski <pawel@kaniewski.eu>
2023-07-27 18:11:59 -0500

    - Start creating details page - Create project with tests

---
+++ HospitalManagement/Business/Handlers/Queries/GetHospitalQueryHandler.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using HospitalManagement.Business.Queries;
+using HospitalManagement.Domain.Dto;
+using HospitalManagement.Infrastructure;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagement.Business.Handlers.Queries
+{
+    public class GetHospitalQueryHandler : IRequestHandler<GetHospital, HospitalData>
+    {
+        private readonly HospitalsDb _db;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+
+        public GetHospitalQueryHandler(HospitalsDb db, IMapper mapper, ILogger<GetHospitalQueryHandler> logger)
+        {
+            _db = db;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+
+        public Task<HospitalData> Handle(GetHospital request, CancellationToken cancellationToken)
+        {
+            var hospital = 
+                _db.Hospitals.
+                    Include(h => h.Address).
+                    SingleOrDefault(h => h.Id == request.HospitalId)
+                ;
+            if (hospital == null)
+            {
+                _logger.LogWarning($"No hotel was found with requested Id: ${request.HospitalId}");
+            }
+            return Task.FromResult(_mapper.Map<HospitalData>(hospital));
+        }
+    }
+}
+
---
+++ HospitalManagement/Business/Queries/GetHospital.cs
@@ -0,0 +1,11 @@
+﻿using HospitalManagement.Domain.Dto;
+using MediatR;
+
+namespace HospitalManagement.Business.Queries
+{
+    public class GetHospital : IRequest<HospitalData>
+    {
+        public Guid HospitalId { get; set; }
+    }
+}
+
